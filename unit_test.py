da2213082e5154387c4bd715eb88473eee95faa6
Rasmus Ansin <rasmus.ansin@gmail.com>
2015-05-17 00:22:37 +0200

    Added more unit tests

--- unit_test.py
+++ unit_test.py
@@ -5,13 +5,17 @@ def performUnitTests():
     print "#"*79
     
     import xmlwikiparser2
-    xmlwikiparser2.test(verbose=False)
+    xmlwikiparser2.test(verbose=True)
+    print "#"*39
     
     import attribute_value_parser
     attribute_value_parser.test(verbose=False)
+    print "#"*39
 
     import attribute_key_parser
     attribute_key_parser.test(verbose=False)
+    print "#"*39
+    
     
     print "#"*79
     print "UNIT TESTING COMPLETED"
