66a5d98497f6f50b6885d978065f67a6d347b9a6
Rasmus Ansin <rasmus.ansin@gmail.com>
2015-05-16 16:58:57 +0200

    Updated attribute_key_parser.py

--- attribute_key_parser.py
+++ attribute_key_parser.py
@@ -12,29 +12,57 @@ class AttributeKeyParser:
         self.keyList = keyList
 
         ##DO STUFF HERE
+        
+        self.valueToSave = "hejsan"
 
     def findNewKey(self, key):
         """Takes a key as a string, and changes it to be more generic
         E.g. "date of birth" -> "birthdate"
         """
-        return key
+        newKey = key
+        
+        
+        key = key.replace("_", "-").replace(" ", "-")
+        
+        match = re.match("(.+)-of-(.+)", key)
+        if match and len(match.groups()) == 2:
+            newKey = match.groups(2) + match.groups(1)
+        
+        return newKey
     
    
-#~ def test():
-#~     print "Testing AttributeKeyParser..."
-#~     
-#~     keyList = ["date of birth", "date_of_birth", "birthdate", "birthdate"]
-#~     attributeKeyParser = AttributeKeyparser(keyList)
-#~     
-#~     f = attributeKeyParser.findNewKeys
-#~    
-#~     for value, 
-#~     assert(f() == "")
-#~     
-#~     print "#"*20
-#~     print "SUCCESSFULLY TESTED AttributeKeyParser"
-#~     print "#" * 20
+def test():
+    print "Testing AttributeKeyParser..."
+    
+    keyList = ["date of birth", "date_of_birth", "birthdate", "birthdate"]
+    attributeKeyParser = AttributeKeyParser(keyList)
+    
+    f = attributeKeyParser.findNewKey
+    
+    inValues = ["dateofbirth",
+            "date of birth",
+            "date_of_birth",
+            "date-of-birth",
+            "date_of birth",
+            ]
+
+    outValues = ["dateofbirth"
+            "birthdate",
+            "birthdate",
+            "birthdate",
+            "birthdate",
+            ]
+   
+    for inValue, outValue in zip(inValues, outValues):
+        print inValue
+        print "->"
+        print outValue
+        print
+        assert(f(inValue) == outValue)
+    
+    print "#"*20
+    print "SUCCESSFULLY TESTED AttributeKeyParser"
+    print "#" * 20
     
 if __name__ == "__main__":
-    pass
-    #~ test()
+    test()
