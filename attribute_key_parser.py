659818b40a3212a809f54ea39b318ab15321111e
Rasmus Ansin <rasmus.ansin@gmail.com>
2015-05-17 20:28:55 +0200

    Made attribute_key_parser.py use an external java call

--- attribute_key_parser.py
+++ attribute_key_parser.py
@@ -2,25 +2,48 @@
 import re
 
 from termcolor import colored
+import os
 
 class AttributeKeyParser:
     """Class that is used to clean keys
     """
-    def __init__(self, keyCounter):
+    #def __init__(self, keyCounter):
+    def __init__(self, keyTranslationFileName, verbose=False):
         """Parses a list of keys and tries to find patterns to be used to
         clean keys.
-        
-        keyList is a raw list of attribute keys (including e.g. duplicates)
-        keyCounter is a collections.Counter object, where the keys
-        (keyCounter.keys()) are the attribute keys and the values
-        (e.g. keyCounter["name"]) are the total number of occurances of
-        that attribute key.
         """
-        self.keyCounter = keyCounter
 
-        ##DO STUFF HERE
+        #Calls Java code
+        cleanedKeysFileName = "attribute_keys_cleaned.txt"
+        try:
+            cmd = "java attribute_key_cleaner %s %s" % \
+                    (keyTranslationFileName, cleanedKeysFileName)
+            if verbose: print "Trying to run command '%s'" % cmd
+            ret = os.system(cmd)
+            if verbose: print "Return value from command: %s" % ret
+            if ret != 0:
+                raise
+        except:
+            print colored("Problem calling '%s', quitting..." % cmd, "yellow")
+            exit()
+            
+        print "(presumably) successfully saved cleaned file as %s" % \
+            cleanedKeysFileName
+            
+            
+        #Loads file
+        self.translationDict = {}
+        try:
+            with open(cleanedKeysFileName) as f:
+                for line in f:
+                    raw, cleaned = line.split("\t")
+                    self.translationDict[raw] = cleaned
+        except:
+            print colored("Problem reading list of cleaned keys from file",
+                    "yellow")
+            exit()
         
-        self.valueToSave = "hejsan"
+        if verbose: print "Loaded translation dictionary from file"
 
     def findNewKey(self, key):
         """Takes a key as a string, and changes it to be more generic
@@ -42,13 +65,15 @@ class AttributeKeyParser:
 def test(verbose=False):
     print "Testing AttributeKeyParser.findNewKey"
     
-    keyList = ["date of birth", "date_of_birth", "birthdate", "birthdate"]
     import attribute_cleaner
     inputFileName = attribute_cleaner.defaultInputFileName
     infoBoxList = attribute_cleaner.loadInfoBoxList(inputFileName,
-            verbose=False)
+            verbose)
     keyCounter = attribute_cleaner.getKeyCounter(infoBoxList)
-    attributeKeyParser = AttributeKeyParser(keyCounter)
+    keyTranslationFileName = "attribute_keys_raw.txt"
+    attribute_cleaner.saveKeyCounterToFile(keyCounter,
+            keyTranslationFileName, verbose)
+    attributeKeyParser = AttributeKeyParser(keyTranslationFileName, verbose)
     
     inValues = ["dateofbirth",
             "date of birth",
@@ -81,4 +106,4 @@ def test(verbose=False):
     print "Successfully tested AttributeKeyParser.findNewKey"
     
 if __name__ == "__main__":
-    test()
+    test(verbose=True)
