31966421f0f4a5b067eb3bf9008d420b7634e89e
Rasmus Ansin <rasmus.ansin@gmail.com>
2015-05-18 21:57:11 +0200

    Added link to full wikipedia json in full_run.sh

--- src/full_run.sh
+++ src/full_run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 echo Calling xmlwikiparser2.py to output raw infobox values, should take 30-45 minutes with an ssd
-python xmlwikiparser2.py ../debug/enwiki_first35klines.txt ../raw_output/ibs_person_raw.json
+python xmlwikiparser2.py  ../raw_output/ibs_person_raw.json
+#python xmlwikiparser2.py ../debug/enwiki_first35klines.txt ../raw_output/ibs_person_raw.json
 
+#Skip the previous step by getting the json here: https://mega.co.nz/#!YwUlSDRR!EAbguiWFg5ppVBsw5fRGoYQCuBjVvMTOoxTcuwH9I14
 echo Calling javac java_key_cleaner.java
 javac java_key_cleaner.java
 
