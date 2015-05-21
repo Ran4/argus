930da884158aa4e84fa1c0483e14cbe3368b4c2c
Didrik Lundberg <didrikl@kth.se>
2015-05-21 19:57:42 +0200

    Made full_run use new xmlwikiparser2.py command

--- src/full_run.sh
+++ src/full_run.sh
@@ -2,7 +2,8 @@
 #All paths given are relative to runstart in /src/
 echo Calling xmlwikiparser2.py to output raw infobox values, should take 30-45 minutes with an ssd
 
-python xmlwikiparser2.py ../enwiki-20150304-pages-articles-multistream.xml ../raw_output/ibs_person_raw.json
+#python xmlwikiparser2.py ../enwiki-20150304-pages-articles-multistream.xml ../raw_output/ibs_person_raw.json
+python xmlwikiparser2.py full
 #python xmlwikiparser2.py ../debug/enwiki_first35klines.txt ../raw_output/ibs_person_raw.json
 
 #Skip the previous step by getting the json here: https://mega.co.nz/#!YwUlSDRR!EAbguiWFg5ppVBsw5fRGoYQCuBjVvMTOoxTcuwH9I14
@@ -12,5 +13,5 @@ javac java_key_cleaner.java
 echo Calling attribute_cleaner.py ../raw_output/ibs_person_raw.json ../output/infobox_output_cleaned.json ../debug/attribute_keys_cleaned.txt
 python attribute_cleaner.py ../raw_output/ibs_person_raw.json ../output/infobox_output_cleaned.json ../debug/attribute_keys_cleaned.txt
  
-echo Calling statistics.py noshow silent
-python statistics.py noshow silent
+#echo Calling statistics.py noshow silent
+#python statistics.py noshow silent
